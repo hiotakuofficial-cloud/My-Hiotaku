553feb61a92ffc5bad79c0d95e2c1ff2a716663c
root <root@localhost.localdomain>
2025-11-23 05:44:04 +0000

    Add: Smooth bottom nav animations when switching pages via swipe or tap

--- lib/main.dart
+++ lib/main.dart
@@ -59,9 +59,10 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
   int _currentIndex = 0;
   late PageController _pageController;
+  late AnimationController _navAnimationController;
 
   final List<Widget> _screens = [
     HomeScreen(),
@@ -74,12 +75,38 @@ class _MainScreenState extends State<MainScreen> {
   void initState() {
     super.initState();
     _pageController = PageController();
+    _navAnimationController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _navAnimationController.forward();
   }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _navAnimationController.dispose();
     super.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    if (_currentIndex != index) {
+      _navAnimationController.reset();
+      setState(() => _currentIndex = index);
+      _navAnimationController.forward();
+    }
+  }
+
+  void _onNavTap(int index) {
+    HapticFeedback.lightImpact();
+    _navAnimationController.reset();
+    setState(() => _currentIndex = index);
+    _pageController.animateToPage(
+      index,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+    _navAnimationController.forward();
   }
 
   @override
@@ -89,7 +116,7 @@ class _MainScreenState extends State<MainScreen> {
         children: [
           PageView(
             controller: _pageController,
-            onPageChanged: (index) => setState(() => _currentIndex = index),
+            onPageChanged: _onPageChanged,
             children: _screens,
           ),
           _buildBottomNav(),
@@ -143,55 +170,55 @@ class _MainScreenState extends State<MainScreen> {
     bool isSelected = _currentIndex == index;
     
     return GestureDetector(
-      onTap: () {
-        HapticFeedback.lightImpact();
-        setState(() => _currentIndex = index);
-        _pageController.animateToPage(
-          index,
-          duration: Duration(milliseconds: 300),
-          curve: Curves.easeInOut,
-        );
-      },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-        padding: EdgeInsets.symmetric(
-          horizontal: isSelected ? 16 : 12,
-          vertical: 8,
-        ),
-        decoration: BoxDecoration(
-          color: isSelected ? Colors.white : Colors.transparent,
-          borderRadius: BorderRadius.circular(25),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            AnimatedScale(
-              scale: isSelected ? 1.1 : 1.0,
-              duration: Duration(milliseconds: 200),
-              child: Icon(
-                icon,
-                color: isSelected ? Colors.black : Colors.white.withOpacity(0.6),
-                size: 24,
-              ),
+      onTap: () => _onNavTap(index),
+      child: AnimatedBuilder(
+        animation: _navAnimationController,
+        builder: (context, child) {
+          return AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            padding: EdgeInsets.symmetric(
+              horizontal: isSelected ? 16 : 12,
+              vertical: 8,
             ),
-            if (isSelected) ...[
-              SizedBox(width: 8),
-              AnimatedOpacity(
-                opacity: isSelected ? 1.0 : 0.0,
-                duration: Duration(milliseconds: 200),
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w600,
+            decoration: BoxDecoration(
+              color: isSelected ? Colors.white.withOpacity(_navAnimationController.value) : Colors.transparent,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Transform.scale(
+                  scale: isSelected ? (1.0 + 0.1 * _navAnimationController.value) : 1.0,
+                  child: Icon(
+                    icon,
+                    color: isSelected 
+                        ? Color.lerp(Colors.white.withOpacity(0.6), Colors.black, _navAnimationController.value)
+                        : Colors.white.withOpacity(0.6),
+                    size: 24,
                   ),
                 ),
-              ),
-            ],
-          ],
-        ),
+                if (isSelected) ...[
+                  SizedBox(width: 8 * _navAnimationController.value),
+                  Opacity(
+                    opacity: _navAnimationController.value,
+                    child: Transform.translate(
+                      offset: Offset((1 - _navAnimationController.value) * 10, 0),
+                      child: Text(
+                        label,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ],
+            ),
+          );
+        },
       ),
     );
   }
