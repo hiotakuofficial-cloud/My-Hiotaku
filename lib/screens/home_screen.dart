0a0f6511f1ab890ef41028ed10daf4b58738f2a5
root <root@localhost.localdomain>
2025-12-03 07:17:20 +0000

    Fix syntax error in home_screen.dart
    - Remove extra closing parenthesis at line 616
    - Resolve build compilation error
    - Clean up widget structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -609,7 +609,6 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
                         );
                       },
                     ),
-                    ),
                   ),
                 ),
               ),
