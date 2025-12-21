a61f25ed36cfc6463e35ab8e988a809a40e816cc
root <root@localhost.localdomain>
2025-12-21 20:01:55 +0000

    Add custom notification icons from user
    - Added proper notification icons (ic_stat_1001162272.png) for all densities
    - Updated notification handler to use @drawable/ic_notification
    - Icons are properly designed for Android notifications
    - Should display correctly in both status bar and notification panel

--- lib/notifications/handler/local_notification_handler.dart
+++ lib/notifications/handler/local_notification_handler.dart
@@ -94,7 +94,7 @@ class LocalNotificationHandler {
         showWhen: true,
         enableVibration: true,
         playSound: true,
-        icon: '@mipmap/ic_launcher',
+        icon: '@drawable/ic_notification',
         largeIcon: DrawableResourceAndroidBitmap('@mipmap/ic_launcher'),
         color: const Color(0xFFFF8C00),
         actions: [
@@ -152,7 +152,7 @@ class LocalNotificationHandler {
       showWhen: true,
       enableVibration: true,
       playSound: true,
-      icon: '@mipmap/ic_launcher',
+      icon: '@drawable/ic_notification',
       largeIcon: DrawableResourceAndroidBitmap('@mipmap/ic_launcher'),
       color: const Color(0xFFFF8C00),
     );
