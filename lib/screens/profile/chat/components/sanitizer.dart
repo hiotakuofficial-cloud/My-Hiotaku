a340a50b83b52d1d486ce9a166f7615c76ffaaa7
root <root@localhost.localdomain>
2026-02-22 17:54:09 +0000

    fix: aggressive line break removal - join all broken sentences

--- lib/screens/profile/chat/components/sanitizer.dart
+++ lib/screens/profile/chat/components/sanitizer.dart
@@ -51,9 +51,10 @@ class ResponseSanitizer {
         .replaceAll(RegExp(r'```[^`]*```'), '') // ```code```
         .replaceAll(RegExp(r'`([^`]+)`'), r'$1') // `code`
         
-        // Fix line breaks - remove random mid-sentence breaks
-        .replaceAll(RegExp(r'([a-z,])\s*\n\s*([a-z])'), r'$1 $2') // Join broken sentences
+        // Fix line breaks - aggressive joining
+        .replaceAll(RegExp(r'([a-zA-Z,!?])\s*\n\s*([a-zA-Z])'), r'$1 $2') // Join broken sentences
         .replaceAll(RegExp(r'\n\s+'), '\n') // Remove leading spaces after newline
+        .replaceAll(RegExp(r'\s+\n'), '\n') // Remove trailing spaces before newline
         
         // Clean excessive whitespace
         .replaceAll(RegExp(r'[ \t]+'), ' ') // Multiple spaces/tabs to single space
